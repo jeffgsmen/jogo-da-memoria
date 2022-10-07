bc4d475357bf60e242c0fdbd4c989aac5c1ff26b
Jeff <Jeff@MacBook-Pro-de-Wendy-Silva.local>
2022-10-07 19:28:27 -0300

    trazendo tudo para o app.js

--- flashcard/src/App.js
+++ flashcard/src/App.js
@@ -1,12 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [isPergunta, setIsPergunta] = useState(true)
+  const [count, setCount] = useState(0)
+  const [fim, setfim] = useState(false)
+
+  const perguntas = [ 'exPergunta1','exPergunta2' ]
+  const resposta = ['exResposta1', 'exResposta2']
+  const hadlerButton = () => {
+    setIsPergunta(true);
+    if (count === (perguntas.length -1)) {
+      setfim(true)
+    } else {
+      let soma = count
+      setCount(soma += 1)
+    }
+  }
+
+  const iniciarJogo = () => {
+    setIsPergunta(true)
+    setCount(0)
+    setfim(false)
+  }
+
   return (
     <div className="App">
       <h1>
-          Flash Card  
+          Flash Card
       </h1>
+      <div onClick={ () => setIsPergunta(!isPergunta) }>
+            {!fim ?
+              (isPergunta ? perguntas[count] : resposta[count]) : (
+                'Fim do jogo'
+              )
+            }
+      </div>
+      <br/>
+      {!fim ? (<button onClick={ () => hadlerButton() }>
+            próxima
+      </button>) : <br/> }
+      {fim ? (
+          <button onClick={ () => iniciarJogo() }>
+              JOGAR NOVAMENTE
+        </button>
+        ) : <br/>
+      }
     </div>
   );
 }
