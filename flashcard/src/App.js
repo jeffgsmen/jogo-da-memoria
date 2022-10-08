ff6833397c413ce02413af4e853aac8f6c5be2d1
Jeff <Jeff@MacBook-Pro-de-Wendy-Silva.local>
2022-10-07 21:13:23 -0300

    contando os pontos

--- flashcard/src/App.js
+++ flashcard/src/App.js
@@ -1,17 +1,38 @@
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 import { useState } from 'react'
 
 function App() {
-  const [isDica, setIsDica] = useState(true)
+  const [isDica, setIsDica] = useState('open')
   const [count, setCount] = useState(0)
   const [fim, setfim] = useState(false)
+  const [points, setpoints] = useState(0)
   // https://pt-br.reactjs.org/docs/hooks-state.html
+  
+  const MySwal = withReactContent(Swal)
 
-  const dica = [ 'DICA1', 'DICA2', 'DICA3', 'DICA4']
-  const resposta = ['RESPOSTA1', 'RESPOSTA2', 'RESPOSTA3', 'RESPOSTA4']
+  const palavras = [
+    {
+    open:'Carro',
+    hide:'Car'
+    },
+    {
+    open:'Gato',
+    hide:'Cat'
+    },
+    {
+    open:'Vaca',
+    hide:'Cow'
+    },
+    {
+    open:'Pato',
+    hide:'Duck'
+    }
+  ]
 
   const hadlerButton = () => {
-    setIsDica(true);
-    if (count === (dica.length -1)) {
+    setIsDica('open');
+    if (count === (palavras.length -1)) {
       setfim(true)
     } else {
       let soma = count
@@ -20,33 +41,61 @@ function App() {
   }
 
   const iniciarJogo = () => {
-    setIsDica(true)
+    setIsDica('open')
     setCount(0)
+    setpoints(0)
     setfim(false)
   }
+
+  const clickCard = () => {
+    if (isDica === 'open' && !fim) {
+      MySwal.fire({
+        title: <strong>Como se diz: {script}, em Inglês?</strong>,
+        input: 'text',
+        confirmButtonText:'responder',
+        inputValidator: (value) => {
+          if (value) {
+            setIsDica('hide')
+            console.log(palavras[count]['hide'], value)
+            if(palavras[count]['hide'] === value) {
+              let newPoint = points + 1;
+              setpoints(newPoint)
+            }
+          }
+      }
+      })
+    }
+  }
+
+  const script = palavras[count][isDica]
 
   return (
     <div className="App">
       <h1>
           Flash Card
       </h1>
-      <div className='card' onClick={ () => setIsDica(!isDica) }>
-            {!fim ?
-              (isDica ? dica[count] : resposta[count]) : (
-                'Fim do jogo'
+      <div className='card' onClick={ () => clickCard() }>
+            {!fim ? (script) : (
+                `Você fez ${points} pontos`
               )
             }
       </div>
       <br/>
-      {!fim ? (<button className='btn' onClick={ () => hadlerButton() }>
-            próxima
-      </button>) : <br/> }
+      {!fim ? (<>
+        <button className='btn' onClick={() => hadlerButton()}>
+          próxima
+        </button>
+        <div>
+          Points: {points}
+        </div></>
+      ) : <br/> }
       {fim ? (
           <button className='btn' onClick={ () => iniciarJogo() }>
               início
         </button>
         ) : <br/>
       }
+      <br/>
     </div>
   );
 }
